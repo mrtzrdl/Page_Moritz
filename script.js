4ff67796114503f3c2d41007b9cd959be2ead618
Frannie Ziesemer <frances.ziesemer@gmail.com>
2020-08-05 13:45:54 +0200

    add gallery

--- script.js
+++ script.js
@@ -1,5 +1,86 @@
 console.log("it works");
 
+function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error('No gallery found');
+  }
+  //selecte elements we need
+  const images = Array.from(gallery.querySelectorAll('img'));
+  const modal = document.querySelector('.click-view-outer');
+  const previousButton = modal.querySelector('.previous');
+  const nextButton = modal.querySelector('.next');
+  let currentImage;
+
+  function openModal() {
+    console.info('opening modal..');
+    // first check if modal is alread open 
+    if (modal.matches('.open')) {
+      console.info('modal already open');
+      return;
+    }
+    modal.classList.add('open');
+
+    // event listeners to be bound when we open modal
+    window.addEventListener('keyup', handleKeyUp);
+    nextButton.addEventListener('click', showNextImage);
+    previousButton.addEventListener('click', showPrevImage);
+  }
+
+  function closeModal() {
+    modal.classList.remove('open');
+  }
+
+  function handleKeyUp(event) {
+    if (event.key === 'Escape') closeModal();
+    if (event.key === 'ArrowRight') showNextImage();
+    if (event.key === 'ArrowLeft') showPrevImage();
+  }
+
+  function showNextImage() {
+    showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+    console.log(nextElementSibling);
+  }
+  function showPrevImage() {
+    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+  }
+
+  function handleClickOutside(event) {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+
+  }
+
+
+  function showImage(element) {
+    if (!element) {
+      console.info('no image to show');
+      return;
+    }
+    //update modal with this info
+    console.log(element);
+    modal.querySelector('img').src = element.src;
+    currentImage = element;
+    openModal();
+  }
+
+  //these are the event listeners
+  images.forEach(image => image.addEventListener('click', event => showImage(event.currentTarget)));
+  modal.addEventListener('click', handleClickOutside);
+
+
+}
+const galleryFarn = Gallery(document.querySelector('.galleryFarn'));
+const gallerySalamander = Gallery(document.querySelector('.gallerySalamander'));
+
+
+
+
+
+
+
+
+
 // selects elements that we will use for the click Gallery
 const cardItemSelection = document.querySelectorAll('.card');
 const clickViewOuter = document.querySelector('.click-view-outer');
